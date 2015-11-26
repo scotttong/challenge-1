4e29f32c123ccdd22dd117012fe9eb207cda713d
Scott Tong <scott.tong@gmail.com>
2015-11-26 00:45:11 -0800

    initial commit
    it’s been a while

---
+++ parallax/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  parallax
+//
+//  Created by Scott Tong on 11/25/15.
+//  Copyright © 2015 Scott Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+	@IBOutlet weak var scrollView: UIScrollView!
+	@IBOutlet weak var contentView: UIImageView!
+	@IBOutlet weak var photoView: UIImageView!
+	@IBOutlet weak var photoContainer: UIView!
+	
+	var offset: CGFloat!
+	var photoHeight: CGFloat!
+
+	
+	override func viewDidLoad() {
+		
+		
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		//size of the frame of the scrollview
+		scrollView.frame.size = CGSize(width:view.frame.width, height:view.frame.height)
+
+		//size of the content
+		scrollView.contentSize = CGSize(width: view.frame.width, height: contentView.frame.height + photoView.frame.height + 64)
+		
+		//add scrollView delegate
+		scrollView.delegate = self
+		
+		//original height of the photo
+		photoHeight = CGFloat(photoContainer.center.y)
+		
+		
+
+	
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func scrollViewDidScroll(scrollView: UIScrollView) {
+		
+		let parallax: CGFloat! = 0.25
+		
+		//calculate the offset
+		offset = CGFloat(scrollView.contentOffset.y)
+		print("offset: \(scrollView.contentOffset.y)")
+		
+		//move the photo container
+		photoContainer.center.y = CGFloat(photoHeight + (offset * parallax))
+		print("photo: \(photoContainer.center.y)")
+	
+	}
+	
+
+
+}
+
